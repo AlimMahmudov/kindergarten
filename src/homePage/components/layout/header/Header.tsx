29ff7f189593773a68fd97ed1ca2c99a6aff6a47
Alim Mahmudov <alimmahmudov27@gmail.com>
2024-11-21 18:08:39 +0600

    about

--- src/homePage/components/layout/header/Header.tsx
+++ src/homePage/components/layout/header/Header.tsx
@@ -1,6 +1,9 @@
 "use client";
 import React from "react";
 import { Link as ScrollLink } from "react-scroll";
+import scss from "./Header.module.scss";
+import logo from "@/shared/images/Снимок экрана от 2024-11-20 08-22-24.png";
+import Image from "next/image";
 
 const links = [
   {
@@ -24,27 +27,27 @@ const links = [
 
 const Header = () => {
   return (
-    <header>
+    <header id={scss.Header}>
       <div className="container">
-        <div className="">
-          <div className="">
-            <ul>
-              {links.map((item, index) => (
-                <li key={index}>
-                  <ScrollLink
-                    activeClass="active"
-                    to={item.link}
-                    spy={true}
-                    smooth={true}
-                    offset={0}
-                    duration={700}
-                  >
-                    {item.name}
-                  </ScrollLink>
-                </li>
-              ))}
-            </ul>
+        <div className={scss.header}>
+          <Image src={logo} alt="" />
+          <div className={scss.nav}>
+            {links.map((item, index) => (
+              <button key={index}>
+                <ScrollLink
+                  activeClass="active"
+                  to={item.link}
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={700}
+                >
+                  {item.name}
+                </ScrollLink>
+              </button>
+            ))}
           </div>
+          <button>add</button>
         </div>
       </div>
     </header>
--- src/homePage/components/pages/HomePage.tsx
+++ src/homePage/components/pages/HomePage.tsx
@@ -3,13 +3,17 @@ import Home from "./site/home/Home";
 import Filials from "./site/filials/Filials";
 import Offer from "./site/offer/Offer";
 import Message from "./site/message/Message";
+import Slider from "./site/slider/Slider";
+import About from "./site/about/About";
 
 const HomePage = () => {
   return (
     <div>
       <Home />
       <Offer />
+      <About />
       <Filials />
+      {/* <Slider /> */}
       <Message />
     </div>
   );

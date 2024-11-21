41e9e5b87eaf0d3cf9c569b46cb352f49893c26b
Alim Mahmudov <alimmahmudov27@gmail.com>
2024-11-21 23:56:43 +0600

    kids

--- src/homePage/components/pages/site/home/Home.tsx
+++ src/homePage/components/pages/site/home/Home.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import scss from "./Home.module.scss";
+// import img from "../../../../../shared/images/Benefits of creative art activities for kids _ Abrakadoodle.jpeg"
+// import img from "@/shared/images/Benefits of creative art activities for kids _ Abrakadoodle.jpeg"
 
 const Home = () => {
   return (
     <div id="home" className={scss.Home}>
       <div className="container">
         <div className={scss.home}>
-          <div className={scss.home_text}>
+          <div className={scss.text}>
             <h1>Мы ждем вас в ДЕТСКИЙ САД УМНАЯ ПЛАНЕТА</h1>
             <p></p>
             <button></button>
           </div>
+          <div className={scss.img}></div>
         </div>
       </div>
     </div>

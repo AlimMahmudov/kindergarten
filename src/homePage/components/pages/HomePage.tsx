57e119c1d5a441e07a1d5e3886765dbd2f6c97d4
AsimMahmudov <AsimMahmudov>
2024-11-24 00:30:44 +0600

    asim

--- src/homePage/components/pages/HomePage.tsx
+++ src/homePage/components/pages/HomePage.tsx
@@ -1,19 +1,32 @@
-import React from "react";
+'use client'
+import React, { useEffect } from "react";
 import Home from "./site/home/Home";
 import Filials from "./site/filials/Filials";
-// import Offer from "./site/offer/Offer";
+import Offer from "./site/offer/Offer";
 import Message from "./site/message/Message";
 import About from "./site/about/About";
 import Videos from "./site/videos/Videos";
 import Answer from "./site/answer/Answer";
 import Circle from "./site/circle/Circle";
 import Sliders from "./site/slider/Slider";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 const HomePage = () => {
+
+
+	useEffect(() => {
+    AOS.init({
+      duration: 1000,  
+      once: true,     
+    });
+  }, []);
+
   return (
     <div>
       <Home />
-      {/* <Offer /> */}
+      <Offer />
       <About />
       <Circle />
       <Sliders />

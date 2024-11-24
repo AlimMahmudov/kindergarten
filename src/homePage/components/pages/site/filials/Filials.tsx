6c0633677aae02ec34a9b8ca026e67ff9cb85ced
Alim Mahmudov <alimmahmudov27@gmail.com>
2024-11-24 15:54:06 +0600

    flials

--- src/homePage/components/pages/site/filials/Filials.tsx
+++ src/homePage/components/pages/site/filials/Filials.tsx
@@ -7,132 +7,90 @@ import bulut from "@/shared/images/bulut.png";
 
 import Image from "next/image";
 
+const data = [
+  {
+    id: 1,
+    name: "Филиал 1",
+    num: "+996 704 42 80 02",
+    eje: "Махмудова Мариям Чынгызовична",
+    doljnost: "Директор садика",
+    doljnost2: "зам Директор",
+    zam: "Махмудова Мариям Чынгызовична",
+    ul: "МусаДжалиля 225",
+  },
+  {
+    id: 2,
+    name: "Филиал 2",
+    num: "+996 704 42 80 02",
+    eje: "Махмудова Мариям Чынгызовична",
+    doljnost: "Директор садика",
+    doljnost2: "зам Директор",
+    zam: "Махмудова Мариям Чынгызовична",
+    ul: "МусаДжалиля 129",
+  },
+  {
+    id: 3,
+    name: "Филиал 3",
+    num: "+996 704 42 80 02",
+    eje: "Махмудова Мариям Чынгызовична",
+    doljnost: "Директор садика",
+    doljnost2: "зам Директор",
+    zam: "Махмудова Мариям Чынгызовична",
+    ul: "МусаДжалиля 110",
+  },
+];
+
 const Filials = () => {
-	return (
-		<>
-			<div id="filials" className={scss.Filials}>
-				<div className="container">
-					<div className={scss.filials}>
-						<div className={scss.filials_text}>
-							<h1 data-aos="fade-left">Филиалы по Бишкеку</h1>
-						</div>
+  return (
+    <>
+      <div id="filials" className={scss.Filials}>
+        <div className="container">
+          <div className={scss.filials}>
+            <div className={scss.filials_text}>
+              <h1 data-aos="fade-left">Филиалы по Бишкеку</h1>
+            </div>
 
-						<div className={scss.block} data-aos="fade-up">
-							<div className={scss.box}>
-								<div className={scss.box_text}>
-									<h1>Филиал 1</h1>
-									<p>+996 704 42 8002</p>
-								</div>
-								<div className={scss.user}>
-									<Image src={teacher} alt="" />
-
-									<div className={scss.user_text}>
-										<p>Директор садика</p>
-										<h1>
-											Махмудова Мариям <br /> Чынгызовична
-										</h1>
-									</div>
-								</div>
-								<div className={scss.user}>
-									<Image src={teacher} alt="" />
-									<div className={scss.user_text}>
-										<p>Директор садика</p>
-										<h1>
-                    Махмудова Мариям <br /> Чынгызовична
-										</h1>
-									</div>
-								</div>
-								<div className={scss.gis}>
-									<div className={scss.addres}>
-										<Image src={gis} alt="" />
-										<h1>Ул.Салиева, 181</h1>
-									</div>
-									<div className={scss.watsapp}>
-										<h2>
-											<IoLogoWhatsapp />
-										</h2>
-									</div>
-								</div>
-							</div>
-
-							<div className={scss.box}>
-								<div className={scss.box_text}>
-									<h1>Филиал 2</h1>
-									<p>+996 704 42 8002</p>
-								</div>
-								<div className={scss.user}>
-									<Image src={teacher} alt="" />
-									<div className={scss.user_text}>
-										<p>Директор садика</p>
-										<h1>
-                    Махмудова Мариям <br /> Чынгызовична
-										</h1>
-									</div>
-								</div>
-								<div className={scss.user}>
-									<Image src={teacher} alt="" />
-									<div className={scss.user_text}>
-										<p>Директор садика</p>
-										<h1>
-                    Махмудова Мариям <br /> Чынгызовична
-										</h1>
-									</div>
-								</div>
-								<div className={scss.gis}>
-									<div className={scss.addres}>
-										<Image src={gis} alt="" />
-										<h1>Ул.Гагарина,135</h1>
-									</div>
-									<div className={scss.watsapp}>
-										<h2>
-											<IoLogoWhatsapp />
-										</h2>
-									</div>
-								</div>
-							</div>
-
-							<div className={scss.box}>
-								<div className={scss.box_text}>
-									<h1>Филиал 3</h1>
-									<p>+996 704 42 8002</p>
-								</div>
-								<div className={scss.user}>
-									<Image src={teacher} alt="" />
-									<div className={scss.user_text}>
-										<p>Директор садика</p>
-										<h1>
-                    Махмудова Мариям <br /> Чынгызовична
-										</h1>
-									</div>
-								</div>
-								<div className={scss.user}>
-									<Image src={teacher} alt="" />
-									<div className={scss.user_text}>
-										<p>Директор садика</p>
-										<h1>
-                    Махмудова Мариям <br /> Чынгызовична
-										</h1>
-									</div>
-								</div>
-								<div className={scss.gis}>
-									<div className={scss.addres}>
-										<Image src={gis} alt="" />
-										<h1>Ул.Садыгалиева,30</h1>
-									</div>
-									<div className={scss.watsapp}>
-										<h2>
-											<IoLogoWhatsapp />
-										</h2>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<Image data-aos="fade-up" className={scss.bulut} src={bulut} alt="img" />
-		</>
-	);
+            <div className={scss.block} data-aos="fade-up">
+              {data.map((el, index) => (
+                <div key={index} className={scss.box}>
+                  <div className={scss.box_text}>
+                    <h1>{el.name}</h1>
+                    <p>{el.num}</p>
+                  </div>
+                  <div className={scss.user}>
+                    <Image src={teacher} alt="" />
+                    <div className={scss.user_text}>
+                      <p>{el.doljnost}</p>
+                      <h1>{el.eje}</h1>
+                    </div>
+                  </div>
+                  <div className={scss.user}>
+                    <Image src={teacher} alt="" />
+                    <div className={scss.user_text}>
+                      <p>{el.doljnost2}</p>
+                      <h1>{el.zam}</h1>
+                    </div>
+                  </div>
+                  <div className={scss.gis}>
+                    <div className={scss.addres}>
+                      <Image src={gis} alt="" />
+                      <h1>{el.ul}</h1>
+                    </div>
+                    <div className={scss.watsapp}>
+                      <h2>
+                        <IoLogoWhatsapp />
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <Image data-aos="fade-up" className={scss.bulut} src={bulut} alt="img" />
+    </>
+  );
 };
 
 export default Filials;
--- src/homePage/components/pages/site/videos/Videos.tsx
+++ src/homePage/components/pages/site/videos/Videos.tsx
@@ -7,102 +7,100 @@ import "slick-carousel/slick/slick.css";
 import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
 const Videos = () => {
-	const sliderRef = useRef<Slider | null>(null);
+  const sliderRef = useRef<Slider | null>(null);
 
-	const settings = {
-		dots: false,
-		infinite: true,
-		speed: 500,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		arrows: false,
-		responsive: [
-			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 2,
-				},
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-		],
-	};
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
-	const slides = [
-		{
-			url: "https://www.youtube.com/embed/Ij6TOPUt_wo",
-			title: "Заголовок 1",
-			disc: "Описание 1",
-			data: "12.03.2024",
-		},
-		{
-			url: "https://www.youtube.com/embed/Ij6TOPUt_wo",
-			title: "Заголовок 2",
-			disc: "Описание 2",
-			data: "13.03.2024",
-		},
-		{
-			url: "https://www.youtube.com/embed/Ij6TOPUt_wo",
-			title: "Заголовок 3",
-			disc: "Описание 3",
-			data: "14.03.2024",
-		},
-		{
-			url: "https://www.youtube.com/embed/Ij6TOPUt_wo",
-			title: "Заголовок 4",
-			disc: "Описание 4",
-			data: "15.03.2024",
-		},
-		{
-			url: "https://www.youtube.com/embed/Ij6TOPUt_wo",
-			title: "Заголовок 4",
-			disc: "Описание 4",
-			data: "15.03.2024",
-		},
-	];
+  const slides = [
+    {
+      url: "https://www.youtube.com/embed/Ij6TOPUt_wo",
+      title: "Заголовок 1",
+      disc: "Описание 1",
+      data: "12.03.2024",
+    },
+    {
+      url: "https://www.youtube.com/embed/Ij6TOPUt_wo",
+      title: "Заголовок 2",
+      disc: "Описание 2",
+      data: "13.03.2024",
+    },
+    {
+      url: "https://www.youtube.com/embed/Ij6TOPUt_wo",
+      title: "Заголовок 3",
+      disc: "Описание 3",
+      data: "14.03.2024",
+    },
+    {
+      url: "https://www.youtube.com/embed/Ij6TOPUt_wo",
+      title: "Заголовок 4",
+      disc: "Описание 4",
+      data: "15.03.2024",
+    },
+    {
+      url: "https://www.youtube.com/embed/Ij6TOPUt_wo",
+      title: "Заголовок 4",
+      disc: "Описание 4",
+      data: "15.03.2024",
+    },
+  ];
 
-	return (
-		<div className={scss.Sliders}>
-			<div className="container">
-				<div className={scss.filials_text}>
-					<h1 data-aos="fade-left">Видео</h1>
-				</div>
-				<div className={scss.sliders}>
-          
-					<button
-						className={`${scss.button} ${scss.prev}`}
-						onClick={() => sliderRef.current?.slickPrev()}>
-						<FaChevronLeft />
-					</button>
-					<div className={scss.sliderContainer}>
-						<Slider ref={sliderRef} {...settings}>
-							{slides.map((slide, index) => (
-								<div key={index} className={scss.sliderBox}>
-									<div className={scss.innerBox}>
-										<div className={scss.imageContainer}>
-											<iframe src={slide.url}></iframe>
-										</div>
-										{/* <h1>{slide.title}</h1>
-                    <h2>{slide.disc}</h2>
-                    <p>{slide.data}</p> */}
-									</div>
-								</div>
-							))}
-						</Slider>
-					</div>
-					<button
-						className={`${scss.button} ${scss.next}`}
-						onClick={() => sliderRef.current?.slickNext()}>
-						<FaChevronRight />
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={scss.Sliders}>
+      <div className="container">
+        <div className={scss.filials_text}>
+          <h1 data-aos="fade-left">Видео</h1>
+        </div>
+        <div className={scss.sliders}>
+          <button
+            className={`${scss.button} ${scss.prev}`}
+            onClick={() => sliderRef.current?.slickPrev()}
+          >
+            <FaChevronLeft />
+          </button>
+          <div className={scss.sliderContainer}>
+            <Slider ref={sliderRef} {...settings}>
+              {slides.map((slide, index) => (
+                <div key={index} className={scss.sliderBox}>
+                  <div className={scss.innerBox}>
+                    <div className={scss.imageContainer}>
+                      <iframe src={slide.url}></iframe>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          </div>
+          <button
+            className={`${scss.button} ${scss.next}`}
+            onClick={() => sliderRef.current?.slickNext()}
+          >
+            <FaChevronRight />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Videos;
